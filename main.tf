d0aeb0e049df1a35b765cfe50d5cc10974a7d14f
udaygattuneu <145166875+udaygattuneu@users.noreply.github.com>
2024-03-15 20:36:19 -0400

    Assignment5 terraform v2 (#15)
    * testingassignment5
    
    * terraform_5
    
    * finalcommitforass5
    
    ---------
    
    Co-authored-by: UdayGattu <udaygattu007@gmail.com>

--- main.tf
+++ main.tf
@@ -1,145 +1,192 @@
 provider "google" {
   project = var.project_id
-  region  = var.region
+  region = var.region
 }
-
-provider "google-beta" {
+data "google_compute_zones" "this" {
+  region  = var.region
   project = var.project_id
-  region  = var.region
+}
+# creating vpc
+resource "google_compute_network" "vpc" {
+  name  =  var.vpc_name
+  auto_create_subnetworks = false
+  delete_default_routes_on_create = true
+  routing_mode = var.routing_mode
+}
+# creating subnets
+
+resource "google_compute_subnetwork" "webapp_subnet" {
+  name = "${var.vpc_name}-webapp"
+  region = var.region
+  network = google_compute_network.vpc.id
+ ip_cidr_range = var.webapp_subnet_cidr
+ private_ip_google_access = false
 }
 
-
-resource "google_compute_network" "vpc" {
-  name                    = var.vpc_name
-  auto_create_subnetworks = false
-  routing_mode            = "REGIONAL"
-  delete_default_routes_on_create = true
+resource "google_compute_subnetwork" "db_host" {
+  name = "${var.vpc_name}-db"
+  region = var.region
+  network = google_compute_network.vpc.id
+  ip_cidr_range = var.db_subnet_cidr
+  private_ip_google_access = false
   
 }
 
-resource "google_compute_subnetwork" "webapp_subnet" {
-  name          = "${var.vpc_name}-webapp"
-  region        = var.region
-  network       = google_compute_network.vpc.id
-  ip_cidr_range = var.webapp_subnet_cidr
-}
-
-resource "google_compute_subnetwork" "db_subnet" {
-  name          = "${var.vpc_name}-db"
-  region        = var.region
-  network       = google_compute_network.vpc.id
-  ip_cidr_range = var.db_subnet_cidr 
-}
-
+#creating routes
 resource "google_compute_route" "internet_access" {
-  name             = "${var.vpc_name}-internet-access"
-  network          = google_compute_network.vpc.id
-  dest_range       = "0.0.0.0/0"
+  name = "${var.vpc_name}-internet-access"
+  network = google_compute_network.vpc.id
+  dest_range = "0.0.0.0/0"
   next_hop_gateway = "default-internet-gateway"
-  priority = 1000
-  depends_on = [google_compute_subnetwork.webapp_subnet] 
+  priority = 900
+  depends_on = [ google_compute_subnetwork.webapp_subnet ]
 }
 
-resource "google_compute_firewall" "allow_webapplication" {
-  name    = "${var.vpc_name}-allow-webapplication"
-  network = google_compute_network.vpc.id
 
+resource "google_compute_firewall" "allow_web" {
+  name = "${var.vpc_name}-allow-web"
+  network = google_compute_network.vpc.name
   allow {
     protocol = "tcp"
-    ports    = [var.app_port]
+    ports = [var.app_port]
   }
-
   source_ranges = ["0.0.0.0/0"]
+
 }
 
-resource "google_compute_firewall" "deny_ssh" {
-  name    = "${var.vpc_name}-deny-ssh"
-  network = google_compute_network.vpc.id
+resource "google_compute_firewall" "block_ssh" {
+  name ="${var.vpc_name}-block-ssh"
+  network = google_compute_network.vpc.name
 
   deny {
     protocol = "tcp"
-    ports    = ["22"]
+    ports = ["22"]
+
   }
-
-  source_ranges = ["0.0.0.0/0"]
+  source_ranges = [ "0.0.0.0/0" ]
+  priority = 1000
+  
 }
 
-#  start of private service access
-resource "google_compute_global_address" "default" {
-  provider     = google-beta
-  project      = var.project_id
-  name         = "${var.vpc_name}-psconnect-ip"
-  address_type = "INTERNAL"
-  purpose      = "VPC_PEERING"
-  network      = google_compute_network.vpc.id
-  prefix_length = 16
-}
-
-resource "google_service_networking_connection" "private_vpc_connection" {
-  provider = google-beta
-  network  = google_compute_network.vpc.id
-  service  = "servicenetworking.googleapis.com"
-  reserved_peering_ranges = [google_compute_global_address.default.name]
-}
-
-
-# end of private service access
 
 resource "google_compute_instance" "vm_instance" {
-  name         = var.vm_name  
-  zone         = var.vm_zone
+  name = var.vm_name
+  zone = var.vm_zone
   machine_type = var.vm_machine_type
 
   boot_disk {
     initialize_params {
       image = var.vm_image
-      type  = var.vm_disk_type
-      size  = var.vm_disk_size_gb
+      type = var.vm_disk_type
+      size = var.vm_disk_size_gb
     }
   }
-
   network_interface {
-    network    = google_compute_network.vpc.id
     subnetwork = google_compute_subnetwork.webapp_subnet.id
+    # network = google_compute_network.this.name
 
-    access_config {
-      // Assigns a public IP address
-    }
+      access_config{
+    
   }
+  }
+  service_account {
+    scopes = ["cloud-platform"]
+    email = var.email
+  }
+
+ metadata = {
+    startup-script = "#!/bin/bash\n  cat<<EOF>/opt/web-app/.env\n  DB_HOST = ${google_sql_database_instance.cloudsql_instance.private_ip_address}\n  DB_NAME = webapp\n  DB_USER= webapp\n  DB_PASSWORD= ${random_password.password.result}\n  DB_PORT= 3306\n  EOF\n\n  chown csye6225:csye6225 /opt/web-app/.env\n  chmod 600 /opt/web-app/.env\n  systemctl restart web-app\n\n  EOT\n"
+  }
+
 }
 
-#  start of creating cloudsql instance 
+resource "google_project_service" "service_networking" {
+  service = "servicenetworking.googleapis.com"
+  project = var.project_id
+}
+
+resource "google_compute_global_address" "private_ip_range" {
+  provider = google-beta
+  project = var.project_id
+  name = "google-managed-services-${var.vpc_name}"
+  purpose = "VPC_PEERING"
+  address_type = "INTERNAL"
+  prefix_length = 24
+  network = google_compute_network.vpc.id
+}
+
+resource "google_service_networking_connection" "private_vpc_connection" {
+  network = google_compute_network.vpc.id
+  service = "servicenetworking.googleapis.com"
+  reserved_peering_ranges = [ google_compute_global_address.private_ip_range.name ]
+  deletion_policy = "ABANDON"
+  # depends_on = [ google_project_service.service_networking ]
+  }
+ 
+
+
 
 resource "google_sql_database_instance" "cloudsql_instance" {
   provider = google-beta
-
-  name             = "your-cloudsql-instance-healthapp" # Ensure this is unique
-  region           = var.region
-  database_version = "MYSQL_5_7" # Example, adjust as needed
-  project          = var.project_id
+  project = var.project_id
+  # name = "${var.cloudsql_instance_name}"
+  name = var.cloudsql_instance_name
+  database_version = var.cloudsql_database_version
+  region = "${var.region}"
   deletion_protection = false
 
   settings {
-    tier = "db-f1-micro" # Adjust as needed, example tier
-
+    tier = var.cloudsql_instance_tier
     ip_configuration {
       ipv4_enabled = false
-      private_network = google_compute_global_address.default.network
-      require_ssl = true # If you want to enforce SSL
+      private_network = google_compute_network.vpc.self_link
+      # require_ssl = var.cloudsql_require_ssl
+      enable_private_path_for_google_cloud_services = true
     }
-
-    disk_type = "PD_SSD"
-    disk_size = 100
-    disk_autoresize = true # Optional, based on your preference
+    disk_type = var.cloudsql_disk_type
+    disk_size = var.cloudsql_disk_size
+    disk_autoresize = var.cloudsql_disk_autoresize
+    backup_configuration {
+      enabled = true
+      binary_log_enabled = true
+    }
     availability_type = "REGIONAL"
 
-  }
+    # database_flags {
+    #    name  = "log_bin"
+    #    value = "ON"
+    # }
+    #     database_flags {
+    #     name  = "binlog_format"
+    #     value = "ROW"
+    #   }
 
-  # This depends_on is crucial to ensure the network settings are in place before the instance is created
+  }
   depends_on = [google_service_networking_connection.private_vpc_connection]
 }
 
 
+resource "google_sql_database" "webapp_database" {
+  name = "webapp"
+  instance = google_sql_database_instance.cloudsql_instance.name
+}
 
+
+resource "random_password" "password" {
+  length = 16
+  special = true
+  override_special = "!#$%&*()-_=+[]{}<>:?"
+}
+
+resource "google_sql_user" "user" {
+  name = "webapp"
+  instance = google_sql_database_instance.cloudsql_instance.name
+  password = random_password.password.result
+  host = "%"
+}
+
+ output "cloudsql_private_ip" {
+    value = "google_sql_database_instance.cloudsql_instance.ip_address"
+  }
 
 
--- variable.tf
+++ variable.tf
@@ -58,3 +58,57 @@ variable "app_port" {
   description = "The application port to allow through the firewall"
   type        = string 
 }
+
+variable "cloudsql_instance_name" {
+  description = "The name of the Cloud SQL instance"
+  type        = string
+}
+
+variable "cloudsql_database_version" {
+  description = "The database version for the Cloud SQL instance"
+  type        = string
+  default     = "MYSQL_5_7"
+}
+
+variable "cloudsql_instance_tier" {
+  description = "The tier (machine type) for the Cloud SQL instance"
+  type        = string
+  default     = "db-f1-micro"
+}
+
+variable "cloudsql_require_ssl" {
+  description = "Whether SSL connections are required for the Cloud SQL instance"
+  type        = bool
+  default     = true
+}
+
+variable "cloudsql_disk_type" {
+  description = "The disk type for the Cloud SQL instance"
+  type        = string
+  default     = "PD_SSD"
+}
+
+variable "cloudsql_disk_size" {
+  description = "The disk size for the Cloud SQL instance in GB"
+  type        = number
+  default     = 100
+}
+
+variable "cloudsql_disk_autoresize" {
+  description = "Whether the Cloud SQL instance disk should be auto-resizable"
+  type        = bool
+  default     = true
+}
+
+variable "db_port" {
+  description = "value"
+  type = number
+  default = 3306
+  
+}
+variable "routing_mode" {
+  description = "value"
+}
+variable "email" {
+  description = "service_email"
+}
